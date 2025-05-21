648358f6ce019027ba156dd5e2b1448faf293162
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-05-21 15:34:24 +0300

    تحسين

--- Barcode Reader/BarcodeScanner.cs
+++ Barcode Reader/BarcodeScanner.cs
@@ -2,62 +2,74 @@
 using AForge.Video.DirectShow;
 using ZXing;
 using System.Linq;
-using Barcode_Reader;
+using System.Drawing;
 
-public class BarcodeScanner
+namespace Barcode_Reader
 {
-    private FilterInfoCollection videoDevices;
-    private VideoCaptureDevice videoSource;
-    private bool isScanning = false;
-
-    private readonly BarcodeReader reader;
-
-    public BarcodeScanner()
+    public class BarcodeScanner
     {
-        reader = new BarcodeReader();
-    }
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private bool isScanning = false;
 
-    public string[] Init()
-    {
-        videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        private readonly BarcodeReader reader;
 
-        return videoDevices.Cast<FilterInfo>().Select(f => f.Name).ToArray();
-    }
-
-    public void Start(int i = 0)
-    {
-        videoSource = new VideoCaptureDevice(videoDevices[i].MonikerString);
-        videoSource.Start();
-    }
-
-    public void Resume()
-    {
-        videoSource.NewFrame += Video_NewFrame;
-        isScanning = true;
-    }
-
-    private void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
-    {
-        if (!isScanning) return;
-        var result = reader.Decode(eventArgs.Frame);
-
-        if (result != null)
-            Form1.Form.Execute(result.Text);
-    }
-
-    public void Pause()
-    {
-        isScanning = false;
-        videoSource.NewFrame -= Video_NewFrame;
-    }
-
-    public void Stop()
-    {
-        if (videoSource != null && videoSource.IsRunning)
+        public BarcodeScanner()
         {
-            videoSource.SignalToStop();
-            videoSource.WaitForStop();
-            videoSource = null;
+            reader = new BarcodeReader();
+        }
+
+        public string[] Init()
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            return videoDevices.Cast<FilterInfo>().Select(f => f.Name).ToArray();
+        }
+
+        public void Start(int i = 0)
+        {
+            videoSource = new VideoCaptureDevice(videoDevices[i].MonikerString);
+            videoSource.Start();
+        }
+
+        public void Resume()
+        {
+            videoSource.NewFrame += Video_NewFrame;
+            isScanning = true;
+        }
+
+        private bool isProcessing = false;
+        private void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (!isScanning || isProcessing) return;
+            isProcessing = true;
+
+            try {
+                using (Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone())
+                {
+                    Result result = reader.Decode(bitmap);
+                    if (result != null)
+                        Form1.Form.Execute(result.Text);
+                }
+            } catch { }
+
+            isProcessing = false;
+        }
+
+        public void Pause()
+        {
+            isScanning = false;
+            videoSource.NewFrame -= Video_NewFrame;
+        }
+
+        public void Stop()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+            }
         }
     }
 }
